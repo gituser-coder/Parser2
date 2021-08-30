7d244eb6c74ecb7631a0b90bc8c65d5c2ed7ba72
Карасев Антон <haplo383@gmail.com>
2021-08-30 10:15:56 +0700

    HttpGetDownloadFileExample

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,7 +3,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 import java.util.*;
 
 public class Parser {
@@ -11,6 +17,7 @@ public class Parser {
 
     public static void main(String[] args) throws IOException {
         List<Book> bookList = new ArrayList<>();
+        HttpGetDownloadFileExample example = new HttpGetDownloadFileExample();
 
         Document document = Jsoup.connect("http://avalanche.vip/deepworlds.htm").get();
         Elements listBooks = document.getElementsByAttributeValue("class", "item");
@@ -20,12 +27,62 @@ public class Parser {
             String bookName = e.child(0).attr("title");
 
             bookList.add(new Book(url1, bookName));
+ //           example.downloadFile(url1,bookName);
+
         }
 
         System.out.println(bookList);
 
+
+
     }
 
+    public static class HttpGetDownloadFileExample {
+
+        private ReadableByteChannel readableChannelForHttpResponseBody = null;
+        private FileChannel fileChannelForDownloadedFile = null;
+
+        public void downloadFile(String url, String file) throws IOException {
+            try {
+                // Define server endpoint
+                URL robotsUrl = new URL(url);
+                HttpURLConnection urlConnection = (HttpURLConnection) robotsUrl.openConnection();
+
+                // Get a readable channel from url connection
+                readableChannelForHttpResponseBody = Channels.newChannel(urlConnection.getInputStream());
+
+                // Create the file channel to save file
+                FileOutputStream fosForDownloadedFile = new FileOutputStream(file);
+                fileChannelForDownloadedFile = fosForDownloadedFile.getChannel();
+
+                // Save the body of the HTTP response to local file
+                fileChannelForDownloadedFile.transferFrom(readableChannelForHttpResponseBody, 0, Long.MAX_VALUE);
+
+            } catch (IOException ioException) {
+                System.out.println("IOException occurred while contacting server.");
+            } finally {
+
+                if (readableChannelForHttpResponseBody != null) {
+
+                    try {
+                        readableChannelForHttpResponseBody.close();
+                    } catch (IOException ioe) {
+                        System.out.println("Error while closing response body channel");
+                    }
+                }
+
+                if (fileChannelForDownloadedFile != null) {
+
+                    try {
+                        fileChannelForDownloadedFile.close();
+                    } catch (IOException ioe) {
+                        System.out.println("Error while closing file channel for downloaded file");
+                    }
+                }
+
+            }
+        }
+    }
 
     static class Book {
         private final String url;
